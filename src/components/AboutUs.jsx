8feb52167653e53e563ccab72429e9b66adc34d8
DIKAYINSU <dkinsu2010@gmail.com>
2025-07-16 16:51:49 +0530

    16 july updated code

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -12,16 +12,28 @@ function AboutUs() {
       <div className="about-text fade-up">
         {/* <!-- <span className="headingfont">About us</span> --> */}
         
-        <p className="headingfont">About Us</p>
-        <h2>Shaping Tomorrow’s Industries with Advanced FRP Solutions</h2>
+       {/* <span className="text-4xl font-extrabold bg-gradient-to-r from-blue-600 via-purple-500 to-pink-500 bg-clip-text text-transparent">About Us </span> */}
+        <span className="text-4xl font-extrabold bg-gradient-to-r from-blue-600 via-purple-500 to-pink-500 bg-clip-text text-transparent">About Us</span>
+
+        <div className="text-base font-medium text-gray-700 transition-all duration-300 ease-in-out hover:scale-103 hover:text-black">
+        <h2 className='about-sub-heading'>Shaping Tomorrow’s Industries with Advanced FRP Solutions</h2>
         <p><span className="a-bold">Di Kay Insulation Industries Pvt. Ltd.</span> is a trusted manufacturer of <span
             className="">FRP composites and insulation solutions,</span>
-          certified with ISO 9001:2015 and Indian Railways HL3 standards.<br />With Many Decades of experience, we
+          certified with ISO 9001:2015 and Indian Railways HL3 standards </p></div>
+          
+          <div className='tab'/>
+
+           <div className="text-base font-medium text-gray-700 transition-all duration-300 ease-in-out hover:scale-103 hover:text-black">
+          <p>
+             With Many Decades of experience, we
           specialize in precision-engineered components used in electrical
           Machinery, Wind turbines, Hydro Generators, and Industrial Infrastructure. Our products are designed to
           perform in demanding environments—ensuring long-term efficiency, safety, and reliability across critical
-          sectors.</p>
+          sectors.
+          </p>
+         </div>
 
+   <div className="text-base font-medium text-gray-700 transition-all duration-300 ease-in-out hover:scale-103 hover:text-black">
         <h4 className='key-heading'>Key Values and Vision</h4>
         <ul className="features">
           <li className="custom-list-item">
@@ -61,9 +73,10 @@ function AboutUs() {
           </li>
 
         </ul>
+        </div>
 
         <a href="./About.html"
-          className="know-more relative inline-flex items-center justify-center mt-6 h-9 w-32 overflow-hidden border rounded-4xl border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
+          className="know-more relative inline-flex items-center justify-center mt-6 h-9 w-32 overflow-hidden border rounded-4xl border-[#25a5f0] text-[#25a5f0] shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-[#25a5f0] before:duration-300 before:ease-out hover:text-white hover:shadow-[#25a5f0] hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
           <span className="relative z-10">Know More</span>
         </a>
 
--- src/components/WhatWeDO.jsx
+++ src/components/WhatWeDO.jsx
@@ -2,40 +2,59 @@ import React from 'react'
 
 function WhatWeDO() {
   return (
-   <>
-   
-  <section className="about-section background " id="whatwedo">
-    <div className="about-container ">
+    <>
 
-      <div className="about-text fade-up ">
-        <span className="a-main-heading ">What We Do</span>
+      <section className="about-section background " id="whatwedo">
+        <div className="about-container ">
 
-        <p className=" text-lg mb-8">
-          <br />
-          We specialize in manufacturing advanced <strong>FRP composites</strong> and <strong>custom insulation
-            solutions</strong> designed for performance, durability, and precision across critical industrial sectors.
-        </p>
-        <h2>Core Capabilities</h2>
-        <p>
-          We specialize in FRP sheet manufacturing (GP03, EPGC, G-10, G-11) and offer CNC winding, molding, and
-          pressing, supported by an in-house R&D lab for prototypes and custom components.
-        </p>
+          <div className="about-text fade-up ">
+            {/* <h1 className="text-xl sm:text-xl md:text-xl font-extrabold bg-gradient-to-r from-blue-600 via-purple-500 to-pink-500 bg-clip-text text-transparent tracking-tight leading-tight transition-all duration-500 ease-in-out hover:scale-105">
+              Transform Your Business with Innovation
+            </h1> */}
+            <span className="text-4xl font-extrabold bg-gradient-to-r from-blue-600 via-purple-500 to-pink-500 bg-clip-text text-transparent">What We Do </span>
 
-        <h2>Key Products</h2>
-        <p>Slot Wedges, FRP Air Guides, and Terminal Boards
-          Epoxy Cylinders, Rings, and Insulation Blocks
-          FRP Tubes, Sheets, Core Clamps, and Barrier Boards
-          Components for Large Motors & Wind Turbine Generators</p>
-      </div>
-      <div className="about-image fade-up">
-        <img src="./src/assets/Images/img1.jpeg" alt="About DiKay" />
+            <div className="text-base font-medium text-gray-700 transition-all duration-300 ease-in-out hover:scale-105 hover:text-black">
+              <p className="whatwedo-para">
+                <br />
+                We specialize in manufacturing advanced <strong>FRP composites</strong> and <strong>custom insulation
+                  solutions</strong> designed for performance, durability, and precision across critical industrial sectors.
+              </p>
+            </div>
 
-      </div>
 
-    </div>
-  </section>
+            <div className="text-base font-medium text-gray-700 transition-all duration-300 ease-in-out hover:scale-105 hover:text-black">
+              <h2 className='whatwedo-h2'>Core Capabilities</h2>
+              <p>
+                We specialize in FRP sheet manufacturing (GP03, EPGC, G-10, G-11) and offer CNC winding, molding, and
+                pressing, supported by an in-house R&D lab for prototypes and custom components.
+              </p>
+            </div>
 
-   </>
+
+
+            <div className="text-base font-medium text-gray-700 transition-all duration-300 ease-in-out hover:scale-105 hover:text-black">
+              <h2>Key Products</h2>
+              <p >
+                Slot Wedges, FRP Air Guides, and Terminal Boards
+                Epoxy Cylinders, Rings, and Insulation Blocks
+                FRP Tubes, Sheets, Core Clamps, and Barrier Boards
+                Components for Large Motors & Wind Turbine Generators
+              </p>
+            </div>
+
+          </div>
+
+
+
+          <div className="about-image fade-up">
+            <img src="./src/assets/Images/img1.jpeg" alt="About DiKay" />
+
+          </div>
+
+        </div>
+      </section>
+
+    </>
   )
 }
 
