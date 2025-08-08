659f2d1797d8294e938374a614538ea8f2fac403
DIKAYINSU <dkinsu2010@gmail.com>
2025-08-08 10:53:26 +0530

    office code deployment

--- src/layout/Applayout.jsx
+++ src/layout/Applayout.jsx
@@ -3,11 +3,13 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
 import ScrollToTopButton from '../components/ScrollToTopButton'
+import Scrolltotop from '../components/Scrolltotop'
 
 
 function Applayout() {
   return (
      <>
+     <Scrolltotop/>
      <Navbar/>
       <ScrollToTopButton/>
      <Outlet/>
