dc8f9397cbaf94eed7c0cddc3b8f5468abfb53ca
DIKAYINSU <dkinsu2010@gmail.com>
2025-08-12 17:00:41 +0530

    this is office commit

--- src/Pages/Defence.jsx
+++ src/Pages/Defence.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PageBanner from '../components/PageBanner'
 import FounderImage from '../assets/Images/img1.jpeg'
-import Railbanner from '../Assets/Images/DefenceBanner.jpg'
+import Railbanner from '../Assets/Images/DefenceBanner2.jpg'
 
 function Defence() {
     return (
@@ -11,18 +11,18 @@ function Defence() {
       <div
                className="relative w-full h-[70vh] md:h-[90vh] bg-cover bg-center bg-no-repeat"
                  style={{
-                   backgroundImage: `url(${Railbanner})`
+                   backgroundImage: `url(${Railbanner   })`
                  }}
               
              >
                <div className="absolute inset-0 bg-red bg-opacity-50"></div>
              
                <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4 md:px-20">
-                 <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-4">
-                Wind Energy
+                 <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-black mb-4">
+              Defence
                  </h1>
-                 <p className="text-white text-base sm:text-lg md:text-xl max-w-2xl">
-             Engineered FRP components for wind energy.
+                 <p className="text-black text-base font-medium sm:text-lg md:text-2xl max-w-2xl">
+            Precision FRP solutions for defence applications
                  </p>
                  
                </div>
